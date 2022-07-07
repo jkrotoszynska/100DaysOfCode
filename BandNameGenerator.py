ae8eb2838323c1a66354b4970cd1c26ce7da99c3
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-07 20:55:12 +0200

    first commit

---
+++ BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator.")
+
+city = input("Which city did you gorw up in? \n")
+pet = input("What is the name of your pet? \n")
+
+print("Your band name should be " + city + " " + pet)
