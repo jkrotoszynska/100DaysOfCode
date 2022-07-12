999c23e232869f64f0ecb72f182255acbd3ae2d2
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-12 22:14:29 +0200

    finish

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -30,22 +30,22 @@ scissors = '''
 symbols = [rock, paper, scissors]
 
 user = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors\n"))
-print(symbols[user])
-
-computer = random.randint(0,2)
-computer_sign = symbols[computer]
-print("Computer chose:")
-print(computer_sign)
-
 if(user >= 3 or user < 0):
     print("Wrong number!")
-elif(user == computer):
-    print("It's a draw! Try again...")
-elif(user == 0 and computer == 2):
-    print("You win!")
-elif(user == 2 and computer == 0):
-    print("You lose...")
-elif(user > computer):
-    print("You win!")
-elif(user < computer):
-    print("You lose...")
+else:
+    print(symbols[user])
+    computer = random.randint(0,2)
+    computer_sign = symbols[computer]
+    print("Computer chose:")
+    print(computer_sign)
+
+    if(user == computer):
+        print("It's a draw! Try again...")
+    elif(user == 0 and computer == 2):
+        print("You win!")
+    elif(user == 2 and computer == 0):
+        print("You lose...")
+    elif(user > computer):
+        print("You win!")
+    elif(user < computer):
+        print("You lose...")
