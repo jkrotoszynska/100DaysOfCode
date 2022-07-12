02bfb6b4b24622a6e59968b548568a682f834f8a
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-12 22:28:01 +0200

    start new project

---
+++ PasswordGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the PyPassword Generator!")
+length = input("How many letters would you like in your password? \n")
+symbols = input("How many symbols would you like? \n")
+numbers = input("How many numbers would you like? \n")
+
