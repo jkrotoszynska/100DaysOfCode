59e9ffbdfa7d153b99cb6a8685941302ea735684
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-13 11:59:29 +0200

    add arrays

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,10 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
 print("Welcome to the PyPassword Generator!")
-length = input("How many letters would you like in your password? \n")
-symbols = input("How many symbols would you like? \n")
-numbers = input("How many numbers would you like? \n")
+numb_letters = int(input("How many letters would you like in your password? \n"))
+numb_symbols = int(input("How many symbols would you like? \n"))
+numb_numbers = int(input("How many numbers would you like? \n"))
 
