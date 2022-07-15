c4588a4e0c250613581e2be23480c35d643d15fe
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-15 15:05:45 +0200

    step 1

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,5 @@
+import random
+
 print('''
 888                                                           
 888                                                           
@@ -11,3 +13,23 @@ print('''
                         Y8b d88P                              
                          "Y88P"   
 ''')
+
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+chosen_word = random.choice(word_list)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = input("Guess the letter: ").lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for letter in chosen_word:
+     if letter == guess:
+          print("Right")
+     else:
+          print("Wrong")
