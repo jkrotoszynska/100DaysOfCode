71d9e0a212b36c83c69ce67415f1577118629357
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-15 21:50:24 +0200

    Step 3

--- Hangman.py
+++ Hangman.py
@@ -16,32 +16,27 @@ print('''
                          "Y88P"   
 ''')
 
-#Step 2 
+#Step 3 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 chosen_word = list(chosen_word)
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
+#Create blanks
 display = []
-for i in range(len(chosen_word)):
+for _ in range(len(chosen_word)):
      display.append("_")
 
-print(display)
-guess = input("Guess a letter: ").lower()
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+while display != chosen_word:
+     guess = input("Guess a letter: ").lower()
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+     #Check guessed letter
+     for position in range(len(chosen_word)):
+          if chosen_word[position] == guess:
+               index = position
+               display[index] = guess
 
-for letter in range(len(chosen_word)):
-     if chosen_word[letter] == guess:
-          index = letter
-          display[index] = guess
-
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+     print(display)
+     
+     if(display == chosen_word):
+          print("You win!")
