9f39767bcdc6d40728a8290a4070d801793beb4e
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-15 21:29:35 +0200

    step 2

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,6 @@
+from operator import le
 import random
+from re import A
 
 print('''
 888                                                           
@@ -14,22 +16,34 @@ print('''
                          "Y88P"   
 ''')
 
-#Step 1 
+#Step 2 
 
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
 chosen_word = random.choice(word_list)
+chosen_word = list(chosen_word)
+print(chosen_word)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-guess = input("Guess the letter: ").lower()
+display = []
+for i in range(len(chosen_word)):
+     display.append("_")
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+print(display)
+guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-     if letter == guess:
-          print("Right")
-     else:
-          print("Wrong")
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+for letter in range(len(chosen_word)):
+     if chosen_word[letter] == guess:
+          index = letter
+          display[index] = guess
+
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
