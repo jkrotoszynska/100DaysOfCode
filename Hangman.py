322079779cc48e67dbb7ac3c7d0ed552a568ff66
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-16 00:45:22 +0200

    some little changes

--- Hangman.py
+++ Hangman.py
@@ -71,47 +71,35 @@ stages = ['''
 =========
 ''']
 
-#Step 4
+
 end_of_game = False
-word_list = ["aardvark", "baboon", "camel"]
+word_list = ["aardvark", "baboon", "camel", "cow", "apple", "watermelon", "bathtube", "happy", "unicorn", "button", "pillow"]
 chosen_word = random.choice(word_list)
 chosen_word = list(chosen_word)
 word_length = len(chosen_word)
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
-
 lives = 6
-stage = -1
 
-#Create blanks
 display = []
 for _ in range(len(chosen_word)):
      display.append("_")
 
-print(stages[stage])
 while display != chosen_word:
      guess = input("Guess a letter: ").lower()
 
      if guess not in chosen_word:
           lives -= 1
-          stage -=1
-          print(stages[stage])
+          print(stages[lives])
           
           if lives == 0:
                print("You lose...")
+               print(f"The word was: {''.join(chosen_word)}")
                exit()
 
      for position in range(word_length):
           letter = chosen_word[position]
           if letter == guess:
             display[position] = letter
-
-         
-
-          #TODO-2: - If guess is not a letter in the chosen_word,
-          #Then reduce 'lives' by 1. 
-          #If lives goes down to 0 then the game should stop and it should print "You lose."
 
      print(display)
      
