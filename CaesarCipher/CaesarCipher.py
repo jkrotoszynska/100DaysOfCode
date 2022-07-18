97879cd0353d5e031f6a91a487bd1bbd2d0002dd
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-18 23:42:34 +0200

    add ceasar def

--- CaesarCipher/CaesarCipher.py
+++ CaesarCipher/CaesarCipher.py
@@ -2,12 +2,7 @@ from base64 import decode
 from turtle import position
 from logo import logo
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-print(logo)
-type = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
-message = input("Type your message: \n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def encrypt(message, shift):
     encrypted_message = ""
@@ -27,9 +22,16 @@ def decrypt(message, shift):
         dencrypted_message += new_letter
     print(f"The decided text is {dencrypted_message}")
 
-if(type == "encode"):
-    encrypt(message, shift)
-elif(type == "decode"):
-    decrypt(message, shift)
-else:
-    print("Error")
+def caesar(type):
+    message = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if(type == "encode"):
+        encrypt(message, shift)
+    elif(type == "decode"):
+        decrypt(message, shift)
+    else:
+        print("Error")
+
+print(logo)
+type = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+caesar(type)
