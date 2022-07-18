80b8c3ed3de66d5afc97b9cdd747566f7c6be3da
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-18 23:33:51 +0200

    encrypt and decrypt def

--- CaesarCipher/CaesarCipher.py
+++ CaesarCipher/CaesarCipher.py
@@ -1,3 +1,35 @@
+from base64 import decode
+from turtle import position
 from logo import logo
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 print(logo)
+type = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+message = input("Type your message: \n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(message, shift):
+    encrypted_message = ""
+    for letter in message:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        encrypted_message += new_letter
+    print(f"The encoded text is {encrypted_message}")
+
+def decrypt(message, shift):
+    dencrypted_message = ""
+    for letter in message:
+        position = alphabet.index(letter)
+        new_position = position - shift
+        new_letter = alphabet[new_position]
+        dencrypted_message += new_letter
+    print(f"The decided text is {dencrypted_message}")
+
+if(type == "encode"):
+    encrypt(message, shift)
+elif(type == "decode"):
+    decrypt(message, shift)
+else:
+    print("Error")
