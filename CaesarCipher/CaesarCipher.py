4860391c5368f185f90083a127f120ca631ba549
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-19 17:26:12 +0200

    shift repaired

--- CaesarCipher/CaesarCipher.py
+++ CaesarCipher/CaesarCipher.py
@@ -4,24 +4,6 @@ from logo import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(plain_text, shift_amount):
-    encrypted_message = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        encrypted_message += new_letter
-    print(f"The encoded text is {encrypted_message}")
-
-def decrypt(plain_text, shift_amount):
-    dencrypted_message = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        dencrypted_message += new_letter
-    print(f"The decided text is {dencrypted_message}")
-
 def caesar(start_text, shift_amount, direction):
     end_text=""
 
@@ -29,10 +11,12 @@ def caesar(start_text, shift_amount, direction):
             shift_amount *= -1
 
     for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        end_text += new_letter
+        if letter in start_text:
+            position = alphabet.index(letter)
+            new_letter = alphabet[position + shift_amount]
+            end_text += new_letter
+        else:
+            end_text += letter
         
     print(f"The {direction}d text is {end_text}")
 
@@ -43,6 +27,7 @@ while program:
     type = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
     message = input("Type your message: \n").lower()
     shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
 
     caesar(start_text=message, shift_amount=shift, direction=type)
     again = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
