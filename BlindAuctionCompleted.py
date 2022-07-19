f6e52078be2f1e036256126247f49c4d7f92ff47
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-19 17:42:19 +0200

    new project

---
+++ BlindAuctionCompleted.py
@@ -0,0 +1,16 @@
+print('''
+              ▓▓▒▒                  
+              ▓▓▓▓██                
+            ░░▓▓██▓▓                
+          ░░▒▒▓▓▒▒▓▓                
+          ██▓▓▒▒▒▒▒▒                
+        ▓▓▓▓████    ██              
+          ▒▒██░░      ▒▒▓▓░░        
+  ▒▒▓▓██▓▓██▓▓▓▓██▓▓▓▓    ▓▓▓▓      
+  ▓▓▓▓████████████████        ██▓▓  
+  ░░░░░░░░░░░░░░░░░░░░        ░░░░  
+''')
+print("Welcomde to the secret auction program.")
+name = input("What is your name?: ")
+bid = int(input("What's your bid?: $"))
+decision = input("Are there any other bidders? Type 'yes' or 'no'. \n")
