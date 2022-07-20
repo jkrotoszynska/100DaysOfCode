00815d84abfad9ed948ca796dd6ccfd0d530399d
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-20 19:00:59 +0200

    working...

--- Calculator.py
+++ Calculator.py
@@ -7,3 +7,32 @@ print('''
 | (_| (_| | | (__| |_| | | (_| | || (_) | |   
  \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
  ''')
+
+def add(n1, n2):
+    return n1 + n2
+
+def substract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add,
+    "-" : substract,
+    "*" : multiply,
+    "/" : divide
+    }
+
+first_number = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+chosen_operation = input("Pick an operation: ")
+second_number = int(input("What's the next number?: "))
+result = ...
+decision = input(f"Type 'y' to continue calculating witch {result}, or type 'n' to start a new calculation: ")
