e0a42194f9289ee9efeb9f636a0cbfb694e7bef3
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-22 20:47:01 +0200

    rules

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,5 +1,21 @@
 import art
 
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+###############################################################
+
 print(art.logo)
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
