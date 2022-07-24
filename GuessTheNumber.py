7c6c276ccea8956f6a5714060b99f549a23da74a
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-07-24 13:14:04 +0200

    finish

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,9 +1,43 @@
-#Number Guessing Game Objectives:
+import random
 
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+def check_number(guessing_number, number,attempts):
+    if (guessing_number > number):
+        print("Too high. \n Guess again.")
+        return attempts - 1
+    elif (guessing_number < number):
+        print("Too low. \n Guess again.")
+        return attempts - 1
+    else:
+        print("You got it!")
+
+def difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    if(level == "easy"):
+        return EASY_LEVEL_TURNS
+    elif(level == "hard"):
+        return HARD_LEVEL_TURNS
+
+def game():
+    print("Welcome to the Number Guessing Game!")
+    number = random.randint(1,100)
+    print("I'm thinking of a number betweem 1 and 100.")
+
+    attempts = difficulty()
+    print(f"You have {attempts} attempts remaining to guess the number.")
+
+    guessing_number = 0
+    while guessing_number != number:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        guessing_number = int(input("Make a guess:"))
+        attempts = check_number(guessing_number, number, attempts)
+
+        if attempts == 0:
+            print("You've run out of guesses. You lose...")
+            return
+        elif(guessing_number != number):
+            print("Guess again...")        
+
+game()
