d35815c143099139cbd75da85cc14b8f68980d20
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-08-01 15:01:06 +0200

    first steps

--- HigherLower/HigherLower.py
+++ HigherLower/HigherLower.py
@@ -1,17 +1,31 @@
+from pydoc import describe
 import art
 from game_data import data
 import random
 
 print(art.logo)
-print("Compare A:")
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+while account_a == account_b:
+    account_b = random.choice(data)
+
+# Format the account data into printable format
+name = account_a["name"]
+description = account_a["description"]
+country = account_a["country"]
+print(f"Compare A: {name}, {description}, from {country}")
+
 print(art.vs)
-print("Compare B:")
+
+name = account_b["name"]
+description = account_b["description"]
+country = account_b["country"]
+print(f"Compare B: {name}, {description}, from {country}")
+
+# Ask user for a guess
 print("Who has more followers? Type 'A' or 'B': ")
 
-# Steps:
-# Generate a random account from the game data
-# Format the account data into printable format
-# Ask user for a guess
 # Check if user is correct
 #   Get follower count of each account
 #   Use if statment to check if user is correct
