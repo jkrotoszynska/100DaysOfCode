558db3ea8560459f65860984e1896facea0f259e
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-08-01 14:55:49 +0200

    steps

---
+++ HigherLower/HigherLower.py
@@ -0,0 +1,22 @@
+import art
+from game_data import data
+import random
+
+print(art.logo)
+print("Compare A:")
+print(art.vs)
+print("Compare B:")
+print("Who has more followers? Type 'A' or 'B': ")
+
+# Steps:
+# Generate a random account from the game data
+# Format the account data into printable format
+# Ask user for a guess
+# Check if user is correct
+#   Get follower count of each account
+#   Use if statment to check if user is correct
+# Give user feedback on theri guess
+# Score keeping
+# Make the game repeatable
+# Making account at posiotn B become the next account at position A
+# Clear the screen between rounds
