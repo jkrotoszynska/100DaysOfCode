d60113a64d4670761f9d28c41c07ad0394836435
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-08-03 12:55:34 +0200

    update

--- HigherLower/HigherLower.py
+++ HigherLower/HigherLower.py
@@ -9,6 +9,12 @@ def printable_format(account):
     country = account["country"]
     return(f" {name}, a {description}, from {country}")
 
+def check_answer(guess, acc_a, acc_b):
+    if acc_a > acc_b:
+        return guess == "a"
+    elif acc_a < acc_b:
+        return guess == "b"
+
 print(art.logo)
 
 account_a = random.choice(data)
@@ -20,14 +26,20 @@ print(f"Compare A:{printable_format(account_a)}")
 print(art.vs)
 print(f"Compare B:{printable_format(account_b)}")
 
-decision = input("Who has more followers? Type 'A' or 'B': ")
+score = 0
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
+a_followers_count = account_a['follower_count']
+b_followers_count = account_b['follower_count']
 
-# Check if user is correct
-#   Get follower count of each account
-#   Use if statment to check if user is correct
-# Give user feedback on theri guess
-# Score keeping
+is_correct = check_answer(guess, a_followers_count, b_followers_count)
+
+if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}")
+else:
+    print(f"You lose... Final score: {score}")
+
 # Make the game repeatable
 # Making account at posiotn B become the next account at position A
 # Clear the screen between rounds
