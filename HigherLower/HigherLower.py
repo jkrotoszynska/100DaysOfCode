22103277f2c982c0dcfbaec6fe19261c43cd465c
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-08-03 16:06:44 +0200

    update

--- HigherLower/HigherLower.py
+++ HigherLower/HigherLower.py
@@ -2,6 +2,7 @@ from pydoc import describe
 import art
 from game_data import data
 import random
+import os
 
 def printable_format(account):
     name = account["name"]
@@ -16,30 +17,31 @@ def check_answer(guess, acc_a, acc_b):
         return guess == "b"
 
 print(art.logo)
-
-account_a = random.choice(data)
-account_b = random.choice(data)
-while account_a == account_b:
-    account_b = random.choice(data)
-
-print(f"Compare A:{printable_format(account_a)}")
-print(art.vs)
-print(f"Compare B:{printable_format(account_b)}")
-
 score = 0
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+game_should_continue = True
+account_b = random.choice(data)
 
-a_followers_count = account_a['follower_count']
-b_followers_count = account_b['follower_count']
+while game_should_continue:
+    account_a = account_b
+    while account_a == account_b:
+        account_b = random.choice(data)
 
-is_correct = check_answer(guess, a_followers_count, b_followers_count)
+    print(f"Compare A:{printable_format(account_a)}")
+    print(art.vs)
+    print(f"Compare B:{printable_format(account_b)}")
 
-if is_correct:
-    score += 1
-    print(f"You're right! Current score: {score}")
-else:
-    print(f"You lose... Final score: {score}")
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# Make the game repeatable
-# Making account at posiotn B become the next account at position A
-# Clear the screen between rounds
+    a_followers_count = account_a['follower_count']
+    b_followers_count = account_b['follower_count']
+
+    is_correct = check_answer(guess, a_followers_count, b_followers_count)
+
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+        game_should_continue = True
+        os.system('cls')
+    else:
+        print(f"You lose... Final score: {score}")
+        game_should_continue = False
