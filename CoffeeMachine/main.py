635fa19d555598a749276fb54f4fa6d5d6c3242c
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-08-04 21:58:55 +0200

    add some lines

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -30,17 +30,54 @@ resources = {
     "coffee": 100,
 }
 
-### STEPS
+money = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickles": 0.05,
+    "pennies": 0.01
+}
 
-# Prompt user by asking What would you like?
+def used_ingredients(name_of_coffee):
 
-coffee = input("What would you like? (espresso/latte/cappuccino): ")
+    if name_of_coffee == "latte" or name_of_coffee == "cappuccino":
+        if resources["water"] < MENU[name_of_coffee]["ingredients"]["water"]:
+            print("Sorry there is not enough water")
+        elif resources["milk"] < MENU[name_of_coffee]["ingredients"]["milk"]:
+            print("Sorry there is not enough milk")
+        elif resources["coffee"] < MENU[name_of_coffee]["ingredients"]["coffee"]:
+            print("Sorry there is not enough coffee")
 
-if coffee == "report":
-    print(resources)
+        resources["water"] = resources["water"] - MENU[name_of_coffee]["ingredients"]["water"]
+        resources["milk"] = resources["milk"] - MENU[name_of_coffee]["ingredients"]["milk"]
+        resources["coffee"] = resources["coffee"] - MENU[name_of_coffee]["ingredients"]["coffee"]
 
-# Turn off the Coffee Machine by entering “off” to the prompt
-# Print report
+    elif name_of_coffee == "espresso":
+        if resources["water"] < MENU[name_of_coffee]["ingredients"]["water"]:
+            print("Sorry there is not enough water")
+        elif resources["coffee"] < MENU[name_of_coffee]["ingredients"]["coffee"]:
+            print("Sorry there is not enough coffee")
+
+        resources["water"] = resources["water"] - MENU[name_of_coffee]["ingredients"]["water"]
+        resources["coffee"] = resources["coffee"] - MENU[name_of_coffee]["ingredients"]["coffee"]
+
+
+
+is_on = True
+money = 0
+
+while is_on:
+    coffee = input("What would you like? (espresso/latte/cappuccino): ")
+
+    if coffee == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${money}")
+    elif coffee == "exit":
+        is_on = False
+    elif coffee == "espresso" or coffee == "latte" or coffee == "cappuccino":
+        used_ingredients(coffee)
+
 # Check resources sufficient?
 # Process coins.
 # Check transaction successful?
