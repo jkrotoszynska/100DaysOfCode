7ce856e392fe67da3b2a7b78bf2473d745ff8a54
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-08-09 23:10:40 +0200

    steps

--- CoffeeMachineOOP/main.py
+++ CoffeeMachineOOP/main.py
@@ -2,3 +2,8 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# print report
+# check resources sufficient
+# process coins
+# check transaction successfuly
+# make coffee
