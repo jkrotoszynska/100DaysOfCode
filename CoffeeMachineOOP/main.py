beeca36071c692bbe902323a264e13f10c3f2b9a
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-08-10 21:17:49 +0200

    finish

--- CoffeeMachineOOP/main.py
+++ CoffeeMachineOOP/main.py
@@ -7,3 +7,25 @@ from money_machine import MoneyMachine
 # process coins
 # check transaction successful
 # make coffee
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    elif choice == "off":
+        is_on = False
+
+    elif choice in options:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
