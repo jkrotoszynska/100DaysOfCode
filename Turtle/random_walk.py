20eabeebbd59a9175f0305b2b824d7e720ae26a2
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-03 17:38:08 +0100

    better def

--- Turtle/random_walk.py
+++ Turtle/random_walk.py
@@ -9,12 +9,24 @@ length = 100
 
 colors = ["DarkGreen", "DarkBlue", "DarkOrange", "DarkRed", "Purple", "Pink"]
 
+tim.width(20)
+tim.speed("fast")
+
 def random_walk(steps):
-    for i in range (steps):
+    for _ in range (steps):
         option = random.choice(options)
-        tim.width(20)
         tim.color(random.choice(colors))
         tim.left(angle * option)
         tim.forward(100)
 
-random_walk(50)
+#random_walk(50)
+
+directions = [0, 90, 180, 270]
+
+def better_random_walk(steps):
+    for _ in range (steps):
+        tim.color(random.choice(colors))
+        tim.forward(50)
+        tim.setheading(random.choice(directions))
+
+better_random_walk(100)
