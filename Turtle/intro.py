4f290257169f87bc6fd21bf321fe7ade0d75862a
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-03 16:38:21 +0100

    square

--- Turtle/intro.py
+++ Turtle/intro.py
@@ -2,7 +2,15 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("pink")
+timmy_the_turtle.color("DeepPink")
+
+# square
+def square():
+    for x in range(4):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.left(90)
+
+square()
 
 screen = Screen()
 screen.exitonclick()
