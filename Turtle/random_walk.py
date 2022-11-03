e07af443d1b7f9595045e0342865b21486fac029
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-03 18:37:11 +0100

    random color

--- Turtle/random_walk.py
+++ Turtle/random_walk.py
@@ -11,6 +11,15 @@ colors = ["DarkGreen", "DarkBlue", "DarkOrange", "DarkRed", "Purple", "Pink"]
 
 tim.width(20)
 tim.speed("fast")
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r, g, b)
+    return random_color
+
 
 def random_walk(steps):
     for _ in range (steps):
@@ -19,13 +28,13 @@ def random_walk(steps):
         tim.left(angle * option)
         tim.forward(100)
 
-#random_walk(50)
 
 directions = [0, 90, 180, 270]
 
 def better_random_walk(steps):
     for _ in range (steps):
-        tim.color(random.choice(colors))
+        #tim.color(random.choice(colors))
+        tim.color(random_color())
         tim.forward(50)
         tim.setheading(random.choice(directions))
 
