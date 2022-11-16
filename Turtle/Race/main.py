00b028d05efa6fffe09a1f37bc1bb9adfa78ac1b
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-16 20:05:35 +0100

    changes

--- Turtle/Race/main.py
+++ Turtle/Race/main.py
@@ -1,7 +1,25 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
-screen.setup(500,400)
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+is_race_on = True
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(-230, y_positions)
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
 
 screen.exitonclick()
