a550c6d1732312fa0cf0a40110b42d94d3938ca4
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-16 22:41:40 +0100

    changes

--- Turtle/Race/main.py
+++ Turtle/Race/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
@@ -13,13 +14,25 @@ for turtle_index in range(0,6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
     new_turtle.color(colors[turtle_index])
-    new_turtle.goto(-230, y_positions)
+    new_turtle.goto(-230, y_positions[turtle_index])
     all_turtles.append(new_turtle)
 
 if user_bet:
     is_race_on = True
 
 while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor()>230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
 
 
 screen.exitonclick()
