a94847b62cd67dd2d2b57e9f92553a86e6961150
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-17 15:23:44 +0100

    window

---
+++ SnakeGame/main.py
@@ -0,0 +1,10 @@
+from turtle import Screen, Turtle
+import random
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+
+screen.exitonclick()
