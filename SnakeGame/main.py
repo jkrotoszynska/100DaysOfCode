f273729ef3c60abca821d383edc9d5d24dca9716
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-17 15:31:00 +0100

    snake

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -6,5 +6,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+starting_positnio=[(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positnio:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
 
 screen.exitonclick()
