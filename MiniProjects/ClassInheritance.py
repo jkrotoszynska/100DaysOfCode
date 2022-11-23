3d6e6ee60a698c17a652dfd08252d04ffabd3ae4
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-23 22:22:07 +0100

    notes

--- MiniProjects/ClassInheritance.py
+++ MiniProjects/ClassInheritance.py
@@ -8,11 +8,15 @@ class Animal:
 class Fish(Animal):
     def __init__(self):
         super().__init__()
+        
+    def breathe(self):
+        super().breathe()
+        print("Doing this underwater.")
 
     def swim(self):
         print("Moving in water.")
 
 nemo = Fish()
 nemo.swim() #Moving in water.
-nemo.breathe() #Inhale, exhale
+nemo.breathe() #Doing this underwater.
 print(nemo.eyes) #2
