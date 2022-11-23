397cb14955fa3b2befa09a97b43b9e39afd2ff51
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-23 22:50:02 +0100

    food in snake

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
-import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,7 +22,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
     snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
