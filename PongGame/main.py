46f42d41c136cfedcf6be6aa9cfe11552bb7bfd0
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-24 15:46:18 +0100

    paddle

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,4 +1,28 @@
 #Create screen
+from turtle import Screen
+from paddle import Paddle
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("My Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
 #Create and move a paddle
 #Create another paddle
 #Create the ball and make it move
@@ -6,3 +30,5 @@
 #Detect collision with paddle
 #Detect when paddle misses
 #Keep score
+
+screen.exitonclick()
