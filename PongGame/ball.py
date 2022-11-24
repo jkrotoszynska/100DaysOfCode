a66635d4842ad45490af4559940888370b06bca0
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-24 17:44:51 +0100

    changes

--- PongGame/ball.py
+++ PongGame/ball.py
@@ -17,7 +17,7 @@ class Ball(Turtle):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
-        
+
     def bounce_y(self):
         self.y_move *= -1
 
@@ -29,9 +29,3 @@ class Ball(Turtle):
         self.goto(0, 0)
         self.move_speed = 0.1
         self.bounce_x()
-
-    def end(self):
-        self.reset()
-        # self.goto(0,0)
-        # self.color("black")
-        # self.move_speed = 0
--- PongGame/scoreboard.py
+++ PongGame/scoreboard.py
@@ -18,11 +18,6 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"{self.score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.color("red")
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
     def increase_score(self):
         self.score +=1
         self.clear()
