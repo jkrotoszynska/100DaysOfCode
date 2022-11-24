99a70bdd9c4633185e5003bdf3797992c81f00ab
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-11-24 14:48:30 +0100

    new project

---
+++ PongGame/main.py
@@ -0,0 +1,8 @@
+#Create screen
+#Create and move a paddle
+#Create another paddle
+#Create the ball and make it move
+#Detect collision with wall and bounce
+#Detect collision with paddle
+#Detect when paddle misses
+#Keep score
