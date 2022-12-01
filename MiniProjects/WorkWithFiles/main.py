de0206ad1a754fb2da1b41a17b84aee5d38a49fd
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-01 23:12:06 +0100

    update

--- MiniProjects/WorkWithFiles/main.py
+++ MiniProjects/WorkWithFiles/main.py
@@ -1,3 +1,7 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+
+with open("new_file.txt", mode="a") as file:
+    file.write("\nNew text.")
