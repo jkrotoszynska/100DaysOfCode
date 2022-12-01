106194694bf35775f85212a982e7d74873139519
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-01 21:19:45 +0100

    scoreboard and file

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Arial", 24, "normal")
+FONT = ("Arial", 20, "normal")
 
 
 class Scoreboard(Turtle):
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.goto(0,250)
         self.penup()
         self.color("white")
@@ -15,14 +16,20 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.color("red")
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.color("red")
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score +=1
-        self.clear()
         self.update_scoreboard()
