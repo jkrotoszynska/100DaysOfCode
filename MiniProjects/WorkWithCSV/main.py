57c0aca0f6edb5980ff2eab9b19ad046cd19c0d8
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-05 23:09:00 +0100

    update

--- MiniProjects/WorkWithCSV/main.py
+++ MiniProjects/WorkWithCSV/main.py
@@ -23,11 +23,46 @@ data = pandas.read_csv("weather_data.csv")
 
 temp_list = data["temp"].to_list()
 print(temp_list)
+print("------------------------")
 
 average = sum(temp_list) / len(temp_list)
 print(round(average, 1))
+print("------------------------")
 
+# Get Data in Columns
 print(data["temp"].mean())
+print("------------------------")
 
 max_temp = data["temp"].max()
 print(max_temp)
+print("------------------------")
+
+print(data.condition)
+print("------------------------")
+
+# Get Data in Row
+print(data[data.day == "Monday"])
+print("------------------------")
+
+print(data[data.temp == data.temp.max()])
+print("------------------------")
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+print("------------------------")
+
+monday_temp = int(monday.temp)
+monday_temp_f = monday_temp * 9/5 + 32
+print(monday_temp_f)
+print("------------------------")
+
+# Create a dataframe from scratch
+data_dict = {
+    "students" : ["Amy", "James", "Angela"],
+    "scores" : [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
+print("------------------------")
+
+data.to_csv("new_data.csv")
