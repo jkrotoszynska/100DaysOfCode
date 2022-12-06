8b4a652d20d492ac103a211307b2b02aa781c24e
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-06 11:21:57 +0100

    notes

---
+++ MiniProjects/ListComprehension.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3]
+new_numbers = [n + 1 for n in numbers]
+print(new_numbers)
+
+name = "Angela"
+letters = [letter for letter in name]
+print(letters)
+
+double = [num * 2 for num in range(1,5)]
+print(double)
