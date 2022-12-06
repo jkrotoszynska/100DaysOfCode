f9cac4aaa559e1a7378261c73c3c4ece15816679
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-06 01:19:30 +0100

    update

--- U.S.StatesGame/main.py
+++ U.S.StatesGame/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,5 +7,27 @@ img = "blank_states_img.gif"
 screen.addshape(img)
 
 turtle.shape(img)
+
+data = pandas.read_csv("50_states.csv")
+state_column = data.state.to_list()
+
+game_is_on = True
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+    if answer_state in state_column:
+        print("true!")
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_xy = data[data.state == answer_state]
+        t.goto(int(state_xy.x), int(state_xy.y))
+        t.write(answer_state)
+
+def get_mouse_click_coor(x,y):
+    print(x,y)
+
+#turtle.onscreenclick(get_mouse_click_coor)
+#turtle.mainloop()
 
 screen.exitonclick()
