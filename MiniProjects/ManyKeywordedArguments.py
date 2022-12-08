18e1c11f8848215e3fa819226a61c9387db80f32
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-09 00:55:15 +0100

    kwargs

--- MiniProjects/ManyKeywordedArguments.py
+++ MiniProjects/ManyKeywordedArguments.py
@@ -6,3 +6,11 @@ def calculate(n, **kwargs):
     print(n)
 
 calculate(2, add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+my_car = Car(make="Nissan")
+print(my_car.model)
