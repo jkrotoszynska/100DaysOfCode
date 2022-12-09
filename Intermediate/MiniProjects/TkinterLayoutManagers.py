ca93c27b66923d8a70df56c184b2ec11119e6d6b
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-09 17:20:23 +0100

    grid

--- Intermediate/MiniProjects/TkinterLayoutManagers.py
+++ Intermediate/MiniProjects/TkinterLayoutManagers.py
@@ -1,23 +1,24 @@
 from tkinter import *
 
+#pack(left or right or bottom), place(x,y), grid(column, row)
+
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.place(x=100, y=150)
+my_label.grid(column=0,row=0)
 my_label["text"] = "New Text"
 
 
 def button_clicked():
     my_label.config(text=entry.get())
 button = Button(text="Click me", command=button_clicked)
-button.place(x=0, y=0)
+button.grid(column=1,row=2)
 
 
 entry = Entry(width=30)
-entry.place(x=100, y=0)
-
+entry.grid(column=1,row=1)
 
 window.mainloop()
