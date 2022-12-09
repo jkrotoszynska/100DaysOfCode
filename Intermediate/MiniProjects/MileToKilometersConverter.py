bcf2ddb53e53faf3b4b450f944802c2651f6a20d
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-09 18:00:21 +0100

    simplify

--- Intermediate/MiniProjects/MileToKilometersConverter.py
+++ Intermediate/MiniProjects/MileToKilometersConverter.py
@@ -2,27 +2,27 @@ from tkinter import *
 
 window = Tk()
 window.title("Miles to Kilometer Converter")
-window.minsize(width=400, height=300)
-window.config(padx=100, pady=50)
+window.config(padx=20, pady=20)
 
-miles = Label(text="Miles", font=("Arial", 20))
+miles = Label(text="Miles")
 miles.grid(column=2,row=0)
 
-equal = Label(text="is equal to", font=("Arial", 20))
+equal = Label(text="is equal to")
 equal.grid(column=0,row=1)
 
-km = Label(text="Km", font=("Arial", 20))
+km = Label(text="Km")
 km.grid(column=2,row=1)
 
-def button_clicked():
-    km_value.config(text=float(miles_value.get()) * 1.6)
-button = Button(text="Calculate", command=button_clicked)
+def miles_to_km():
+    km_value.config(text=float(miles_value.get()) * 1.609)
+
+button = Button(text="Calculate", command=miles_to_km)
 button.grid(column=1,row=2)
 
 miles_value = Entry(width=10)
 miles_value.grid(column=1,row=0)
 
-km_value = Label(text="0", font=("Arial", 20))
+km_value = Label(text="0")
 km_value.grid(column=1,row=1)
 
 window.mainloop()
