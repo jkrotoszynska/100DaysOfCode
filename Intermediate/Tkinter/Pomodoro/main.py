e24bac5dad652ac6552162ded41a7c930e220c6b
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-09 19:20:43 +0100

    first step

--- Intermediate/Tkinter/Pomodoro/main.py
+++ Intermediate/Tkinter/Pomodoro/main.py
@@ -24,10 +24,21 @@ window.config(padx=100, pady=100, bg=YELLOW)
 
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file="tomato.png")
-canvas.create_image(101, 111, image=img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.create_image(100, 112, image=img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
+heading = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+heading.grid(column=1, row=0)
+
+checkmark = Label(text="✔", font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=YELLOW)
+checkmark.grid(column=1, row=3)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
 
 
 window.mainloop()
