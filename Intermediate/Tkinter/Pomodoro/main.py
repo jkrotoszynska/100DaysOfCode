26cb2c2ab7e7cb284328e9a5e63e53e118b1a4ea
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-09 18:29:40 +0100

    pomodoro

--- Intermediate/Tkinter/Pomodoro/main.py
+++ Intermediate/Tkinter/Pomodoro/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=100, bg=YELLOW)
+
+canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
+img = PhotoImage(file="tomato.png")
+canvas.create_image(101, 111, image=img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+
+window.mainloop()
