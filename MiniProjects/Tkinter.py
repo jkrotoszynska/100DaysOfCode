142eecd80f53cb5305a0672684ad4fbdbf820843
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-09 16:19:27 +0100

    entry

--- MiniProjects/Tkinter.py
+++ MiniProjects/Tkinter.py
@@ -8,9 +8,17 @@ my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
 my_label["text"] = "New Text"
-my_label.config(text="New Text v2")
 
-button = Button(text="Click me")
+# Button
+def button_clicked():
+    my_label.config(text=input.get())
+
+button = Button(text="Click me", command=button_clicked)
 button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+
 
 window.mainloop()
