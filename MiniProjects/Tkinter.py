eb8b9d5c6fa55fea0d79b7f92cf2bd2d523af2b6
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-09 15:21:17 +0100

    button

--- MiniProjects/Tkinter.py
+++ MiniProjects/Tkinter.py
@@ -1,10 +1,16 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="bottom")
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text v2")
+
+button = Button(text="Click me")
+button.pack()
 
 window.mainloop()
