1a61f114b9073c3860ca48ba1b6c2f253a7a1a61
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-11 02:07:00 +0100

    errors

--- Intermediate/MiniProjects/Errors.py
+++ Intermediate/MiniProjects/Errors.py
@@ -1,11 +1,15 @@
 try:
     file = open("a_file.txt")
     a_dictionary = {"key" : "value"}
-    print(a_dictionary["new_key"])
+    print(a_dictionary["key"])
 
 except FileNotFoundError:
     file = open("a_file.txt", "w")
     file.write("Something")
-    
+
 except KeyError as error_message:
     print(f"The key {error_message} does not exist")
+
+else:
+    content = file.read()
+    print(content)
