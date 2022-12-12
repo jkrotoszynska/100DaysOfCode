0a61f1d8d7bd443ba97b016ed4ea752ac7abc6d2
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-12 21:13:19 +0100

    json with error

--- Intermediate/MyPass/main.py
+++ Intermediate/MyPass/main.py
@@ -39,10 +39,22 @@ def save_data():
     if len(website_data) == 0 or len(password_data) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("saved_data.json", "w") as data_file:
-            json.dump(new_data, data_file, indent=4) 
+        try:
+            with open("data.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
 
-            website.delete(0,END)
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website.delete(0, END)
             password.delete(0, END)
     
         
