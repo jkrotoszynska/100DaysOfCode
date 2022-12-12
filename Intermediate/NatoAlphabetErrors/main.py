3be1d9c7b76f7f1db19dbdb9b6d719425c059b64
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-12 20:20:25 +0100

    small project

--- Intermediate/NatoAlphabetErrors/main.py
+++ Intermediate/NatoAlphabetErrors/main.py
@@ -5,7 +5,15 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-
 word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generate():
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate()
+    else:
+        print(output_list)
+
+generate()
