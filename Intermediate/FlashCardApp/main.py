f16eb52a66f20b3863001940f9e66d7c18b3c7ba
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-13 19:49:40 +0100

    next and flip

--- Intermediate/FlashCardApp/main.py
+++ Intermediate/FlashCardApp/main.py
@@ -8,21 +8,32 @@ random_word = None
 
 # ---------------------------- WORDS GENERATOR ------------------------------- #
 data = pandas.read_csv("data/french_words.csv")
-french_words = data["French"]
+to_learn = data.to_dict(orient="records")
+current_card = {}
 
-def generate():
-    random_word = random.choice(french_words)
+def next_card():
+    global current_card
+    current_card = random.choice(to_learn)
     canvas.itemconfig(title, text="French")
-    canvas.itemconfig(word, text=random_word)
+    canvas.itemconfig(word, text=current_card["French"])
+
+# ---------------------------- FLIP CARD ------------------------------- #
+def flip_card():
+    canvas.itemconfig(title, text="English", fill="white")
+    canvas.itemconfig(word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_img, image=back_img)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
-img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=img)
+front_img = PhotoImage(file="images/card_front.png")
+back_img = PhotoImage(file="images/card_back.png")
+card_img = canvas.create_image(400, 263, image=front_img)
 
 title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
@@ -32,14 +43,14 @@ canvas.grid(column=0, columnspan=2, row=0)
 
 
 yes_img = PhotoImage(file="images/right.png")
-yes_button = Button(image=yes_img, highlightthickness=0, command=generate)
+yes_button = Button(image=yes_img, highlightthickness=0, command=next_card)
 yes_button.config(pady=50)
 no_img = PhotoImage(file="images/wrong.png")
-no_button = Button(image=no_img, highlightthickness=0, command=generate)
+no_button = Button(image=no_img, highlightthickness=0, command=next_card)
 no_button.config(pady=50)
 yes_button.grid(column=1, row=1)
 no_button.grid(column=0, row=1)
 
-generate()
+next_card()
 
 window.mainloop()
