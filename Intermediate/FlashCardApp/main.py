f3f50cdc6e9c7a2c252fc6a3243310048380a76a
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2022-12-13 19:56:36 +0100

    flip works

--- Intermediate/FlashCardApp/main.py
+++ Intermediate/FlashCardApp/main.py
@@ -12,7 +12,8 @@ to_learn = data.to_dict(orient="records")
 current_card = {}
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(title, text="French", fill="black")
     canvas.itemconfig(word, text=current_card["French"], fill="black")
@@ -30,6 +31,7 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 front_img = PhotoImage(file="images/card_front.png")
