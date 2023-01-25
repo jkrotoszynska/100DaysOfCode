7e5a72ec5d288f55c724f9df9e6e1a62cdbe12f5
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2023-01-25 17:30:32 +0100

    datetime

---
+++ IntermediatePlus/MiniProjects/WorkWithDatetime.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year = 2000, month = 11, day = 29)
+print(date_of_birth)
