62e21a9209a4a44fef7f731d402403c6bcd54750
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2023-02-06 22:07:09 +0100

    still working

--- IntermediatePlus/TriviaQuestionAPI/ui.py
+++ IntermediatePlus/TriviaQuestionAPI/ui.py
@@ -1,17 +1,26 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        canvas = Canvas(width=300, height=250)
-        quote_text = canvas.create_text(150, 207, text="HERE", width=250, font=("Arial", 16, "bold"), fill="white")
-        canvas.grid(row=1, column=0, columnspan=2)
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text", 
+            fill=THEME_COLOR, 
+            font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_button_img = PhotoImage(file="./images/true.png")
         true_button = Button(image=true_button_img, highlightthickness=0)
@@ -21,7 +30,13 @@ class QuizInterface:
         false_button = Button(image=false_button_img, highlightthickness=0)
         false_button.grid(column=1, row=2)
 
-        score = Label(text="score", font=("Arial", 16, "bold"))
+        score = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         score.grid(column=1, row=0)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
