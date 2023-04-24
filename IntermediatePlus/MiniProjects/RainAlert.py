e4fb4411dee4c783e7e63423d2d3f0330cf791d0
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2023-04-24 21:41:47 +0200

    done

--- IntermediatePlus/MiniProjects/RainAlert.py
+++ IntermediatePlus/MiniProjects/RainAlert.py
@@ -6,10 +6,23 @@ OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 location_params = {
     "lat": 54.3521,
     "lon": 18.6464,
+    "exclude": "current,minutely,daily",
     "appid": API_KEY
 }
 
 response = requests.get(OWM_Endpoint, params = location_params)
 
-print(response.status_code)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+weather_hourly = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_hourly:
+    id = hour_data["weather"][0]["id"]
+    if int(id) < 700:
+        #print("Bring an umbrella")
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
