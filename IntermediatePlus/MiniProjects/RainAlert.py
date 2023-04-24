62c41df8a09f9d9c966f6e5e0fcae36386b8eb07
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2023-04-24 21:52:32 +0200

    with twilio

--- IntermediatePlus/MiniProjects/RainAlert.py
+++ IntermediatePlus/MiniProjects/RainAlert.py
@@ -1,7 +1,11 @@
+import os
+from twilio.rest import Client
 import requests
 
 API_KEY = ""
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
 location_params = {
     "lat": 54.3521,
@@ -21,8 +25,16 @@ will_rain = False
 for hour_data in weather_hourly:
     id = hour_data["weather"][0]["id"]
     if int(id) < 700:
-        #print("Bring an umbrella")
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an umbrella!",
+        from_=['TWILIO_NUMBER'],
+        to=['MY_NUMBER']
+        )
+    
+    print(message.status)
